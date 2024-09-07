6768da166e3ddf9f2ce9185b5efc59ce00abc8be
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2024-09-07 23:33:22 +0700

    added all

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def show_main(request):
         'products': [
             {
                 'name': 'barcelona 2024',
-                'price': 100,
+                'price': 500,
                 'description': 'this is great product with excellent features!',
                 'image_url': 'https://down-id.img.susercontent.com/file/id-11134207-7r98z-lsh0i1i3mid960'  # Add the image URL here
             },
@@ -18,7 +18,7 @@ def show_main(request):
             {
                 'name': 'real madrid 2024',
                 'price': 150,
-                'description': 'great product excellent features!',
+                'description': 'decent product with good features!',
                 'image_url': 'https://footdealer.co/wp-content/uploads/2023/06/Mailllot-Match-Real-Madrid-Domicile-2023-2024-1.jpg'  # Add the image URL here
             }
         ],
